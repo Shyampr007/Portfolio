64a377aba7b0ac3b801127899779b78f0a9875fa
Shyam Prakash S <shyampr007r@gmail.com>
2026-01-01 15:34:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
-    reveals.forEach(section => {
+    reveals.forEach((section, index) => {
         const top = section.getBoundingClientRect().top;
         if (top < window.innerHeight - 100) {
-            section.classList.add("active");
+            setTimeout(() => {
+                section.classList.add("active");
+            }, index * 150);
         }
     });
+});
+window.addEventListener("load", () => {
+
+    if (typeof particlesJS === "undefined") {
+        console.error("Particles.js not loaded");
+        return;
+    }
+
+    particlesJS("particles-js", {
+        particles: {
+            number: { value: 70 },
+            color: { value: "#38bdf8" },
+            shape: { type: "circle" },
+            opacity: { value: 0.6 },
+            size: { value: 3, random: true },
+            line_linked: {
+                enable: true,
+                distance: 140,
+                color: "#38bdf8",
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                enable: true,
+                speed: 2
+            }
+        },
+        interactivity: {
+            events: {
+                onhover: { enable: true, mode: "repulse" },
+                onclick: { enable: true, mode: "push" }
+            }
+        },
+        retina_detect: true
+    });
+
 });
